53099bacefd0089a5b8aaf627f2716f7d829c739
ThisMSH <midoyugi@hotmail.com>
2023-07-06 15:58:35 +0100

    Setup the server and adding JWT function

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,28 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import connectDB from "./config/Db.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
+connectDB();
 
 const port = process.env.PORT || 5000;
-
 const app = express();
+
+// Using the cookie-parser middleware to parse cookies from incoming requests
+app.use(cookieParser());
+
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
+// Middleware to parse URL-encoded request bodies
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+
+// Middlewares
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
