7398991838a976557674e884f8ecdb3131fadb8f
El Mahdi S.H <midoyugi@hotmail.com>
2023-07-21 21:05:18 +0100

    Adding Sign-In page

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -23,7 +23,27 @@ export default withMT({
             },
             fontFamily: {
                 "cairo": ["Cairo", "sans-serif"]
-            }
+            },
+            dropShadow: {
+                "black-sm": [
+                    "0 2px 4px rgb(0 0 0 / 0.5)"
+                ],
+                "white-custom": [
+                    "0 0px 4px rgb(255 255 255)"
+                ]
+            },
+            screens: {
+                'sm-max': {'max': '639px'},
+                'md-max': {'max': '767px'},
+                'lg-max': {'max': '1023px'},
+                'xl-max': {'max': '1279px'},
+                '2xl-max': {'max': '1535px'},
+                'sm': '640px',
+                'md': '768px',
+                'lg': '1024px',
+                'xl': '1280px',
+                '2xl': '1536px',
+            },
         }
     },
     plugins: [
