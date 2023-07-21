4616dd5243c8b72d35e91647531ed24791b042ff
abhannouni <abdeslam.hannouni@gmail.com>
2023-07-21 18:41:59 +0100

    navbar

---
+++ Frontend/src/components/utils/Avatar.jsx
@@ -0,0 +1,12 @@
+import { Avatar } from "@material-tailwind/react";
+ 
+export default function Example() {
+  return (
+    <Avatar 
+      size="lg" 
+      alt="avatar"
+      src="https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o=" 
+      className="ring-1 ring-white border  shadow-xl "
+    />
+  );
+}
---
+++ Frontend/src/components/utils/Notif.jsx
@@ -0,0 +1,8 @@
+import { Icon } from '@iconify/react';
+
+
+export default function Example() {
+    return (
+          <Icon icon="material-symbols:notifications" color="white"  width="30" height="30" />      
+    );
+  }
